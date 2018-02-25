5401d9422f2f108e511c6c9893a36dccd3987def
wangzhichen <wzc18811741603@126.com>
2018-02-25 21:09:02 +0800

    2

--- src/com/lqn/Hello.java
+++ src/com/lqn/Hello.java
@@ -1,7 +1,11 @@
 package com.lqn;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Hello {
      public static void main(String[] args) {
-		System.out.println("hello");
+    	 System.out.println("hello");
+    	 Lock lock = new ReentrantLock();
 	}
 }
